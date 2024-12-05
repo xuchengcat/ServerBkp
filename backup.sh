38dc97d73cdc3caf33f226af65dbf9340be79642
xuchengcat <xuchengcat@outlook.com>
2024-12-05 15:05:14 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -65,14 +65,16 @@ backup() {
 }
 
 # Perform backups to the common destination
-# backup "$backup_ubuntu" "$common_dest" "Ubuntubkp-$hostname" "$excludefile"
-# backup "$backup_photos" "$common_dest" "Photosbkp-$hostname" ""
-# backup "$backup_sdd128" "$common_dest" "Sdd128bkp-$hostname" ""
+backup "$backup_ubuntu" "$common_dest" "Ubuntubkp-$hostname" "$excludefile"
+backup "$backup_photos" "$common_dest" "Photosbkp-$hostname" ""
+backup "$backup_sdd128" "$common_dest" "Sdd128bkp-$hostname" ""
 
 # List all files in common_dest and store in a variable
 files_list=$(ls "$common_dest" | tr '\n' ',')
 
 # Send Bark notification with the list of files
 curl -s "https://$BARK_SERVER/$BARK_TOKEN/ServerBackupFinished/$files_list?group=Server"
+
+echo "back up succress"
 
 
