5794e9ff7ffb15d13ec11e1fbd413faa0e16cd7f
xuchengcat <xuchengcat@outlook.com>
2024-12-10 16:11:57 +0800

    Update update-iptv.sh

--- iptv-allinone/update-iptv.sh
+++ iptv-allinone/update-iptv.sh
@@ -5,8 +5,8 @@ BARK_TOKEN="iphone" # my device token
 docker stop iptv
 docker rm iptv
 # 拉取最新的镜像并上线
-docker-compose pull youshandefeiyang/allinone:latest
-docker-compose up -d
+docker compose pull youshandefeiyang/allinone:latest
+docker compose up -d
 
 # Send Bark notification with the list of files
 curl -s "https://$BARK_SERVER/$BARK_TOKEN/iptv_update_succress?group=Server"
